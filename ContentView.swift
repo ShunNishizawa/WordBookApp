48f41dce095ea5cb180360933b19654802e586db
NishizawaShun <nishizawa.shun.819@gmail.com>
2021-08-28 23:06:57 +0900

    FirstCommit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordBookApp
+//
+//  Created by 西澤駿 on 2021/08/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
